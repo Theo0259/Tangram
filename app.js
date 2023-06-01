7046d2f00217754c135c886861113465ab51a4dc
Maryam597 <maryam.dri@hotmail.fr>
2023-06-01 14:41:48 +0200

    ok

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ function swapStyleSheet(sheet){
       const body = document.querySelector('body');
       const darkmode = document.getElementById('darkmode');
       const container =document.getElementById('container');
+	  const description =document.getElementById('description');
+
 
       darkmode.onclick = function(){
       body.classList.toggle('active');
       darkmode.classList.toggle('active');
       container.classList.toggle('active');
+	  description.classList.toggle('active');
+
 } 
