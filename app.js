43c6e35dc984b071e4314d8232562d1a1d865ece
Maryam597 <maryam.dri@hotmail.fr>
2023-06-02 09:57:52 +0200

    Group project1

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ function swapStyleSheet(sheet){
       const body = document.querySelector('body');
       const darkmode = document.getElementById('darkmode');
       const container =document.getElementById('container');
-	  const description =document.getElementById('description');
+	const description =document.getElementById('description');
+      const footer1 =document.querySelector('footer');
+
 
 
       darkmode.onclick = function(){
       body.classList.toggle('active');
       darkmode.classList.toggle('active');
       container.classList.toggle('active');
-	  description.classList.toggle('active');
+	description.classList.toggle('active');
+      footer1.classList.toggle('active');
+
 
 } 
